1b9cb20d472117c27e95d3cbc62c156ea8d089c0
Akanopeacemaker <abdulsalamfarouk1802@gmail.com>
2025-10-24 15:21:32 +0100

    lv_update

--- func/func_logi_growth.R
+++ func/func_logi_growth.R
@@ -2,9 +2,11 @@
 #' This function is designed to work with the deSolve::ode function
 func_logi_growth <- function(t, state, parms) {
     # implement this function here
+    dp<-parms["r"]*state["p"]*(1-state["p"]/parms["k"])
+    return(list(c(dp)))
 }
 
-if (FALSE) {
+if (TRUE) {
     out <- deSolve::ode(
         y = c("p" = 10), times = seq(1, 100, 0.1), func = func_logi_growth,
         parms = c("r" = 0.1, "k" = 1000), method = "euler"
